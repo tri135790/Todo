d7c44ec69a54729d9b3d92e5b9f669c4a83f1761
Thanh Tri TRAN <tranthanhtrivn@gmail.com>
2021-08-27 21:12:28 +0700

    Update Login.js

--- todo_client/src/component/Login.js
+++ todo_client/src/component/Login.js
@@ -6,34 +6,63 @@ class LoginComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            username: 'tri135790',
-            password: ''
+            username: '',
+            password: '',
+            isLoginFailed: false,
+            isLoginSuccess: false
         }
-        this.handleUsernameChange = this.handleUsernameChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleLoginClicked=this.handleLoginClicked.bind(this)
     }
 
-    handleUsernameChange(event) {
+    handleChange(event) {
         this.setState({
-            username:event.target.value
+            [event.target.name] :event.target.value
         })
     }
 
-    handlePasswordChange(event) {
-        this.setState({
-            password:event.target.value
-        })
+    handleLoginClicked() {
+        if (this.state.username === 'tri135790' && this.state.password === 'tri123_NK') {
+            this.setState({isLoginFailed:false})
+            this.setState({isLoginSuccess:true})
+        } else {
+            this.setState({isLoginFailed:true})
+            this.setState({isLoginSuccess:false})
+        }
     }
 
     render() {
         return(
             <div>
-            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange} />
-            Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
-            <button> Login</button>
+                {/* <ShowInvalidCredentials isLoginFailed={this.state.isLoginFailed}/> */}
+                {/* <ShowSuccessCredentials isLoginSuccess={this.state.isLoginSuccess}/> */}
+                {this.state.isLoginFailed && <div> Invalid </div>}
+                {this.state.isLoginSuccess && <div> Successfully </div>}
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+            <button onClick={this.handleLoginClicked}> Login</button>
             </div>
         )
     }
+}
+
+function ShowInvalidCredentials(props) {
+    if(props.isLoginFailed) {
+        return <div>
+            Invalid Credentials
+        </div>
+    }
+    return null;
+}
+
+
+function ShowSuccessCredentials(props) {
+    if(props.isLoginSuccess) {
+        return <div>
+            Login Successfully!
+        </div>
+    }
+    return null;
 }
 
 export default LoginComponent;
